c49c311bcbaabf582416a1b3ab91c526cd301bab
dorpuz <dorpuz@hotmail.com>
2019-02-09 14:58:11 +0100

    programmers

--- src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
+++ src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
@@ -2,6 +2,7 @@ package pl.edu.wszib.springhelloworld.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import pl.edu.wszib.springhelloworld.model.EkspresDoKawy;
 import pl.edu.wszib.springhelloworld.model.Komputer;
 import pl.edu.wszib.springhelloworld.model.Kubek;
@@ -9,6 +10,24 @@ import pl.edu.wszib.springhelloworld.model.Programista;
 
 @Configuration
 public class MainConfiguration {
+
+    @Bean
+    @Scope("prototype")
+    public Kubek kubek(){
+        return new Kubek();
+    }
+
+    @Bean
+    public EkspresDoKawy ekspresDoKawy(){
+        return new EkspresDoKawy();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Komputer komputer(){
+        return new Komputer();
+    }
+
 
     @Bean
     public Programista marek(Kubek kubek, Komputer komputer, EkspresDoKawy ekspresDoKawy) {
