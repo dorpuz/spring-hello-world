a7bbbadaa9e0dbefe4f76399d8aee81d437a0924
dorpuz <dorpuz@hotmail.com>
2019-02-09 11:02:51 +0100

    ex1

---
+++ src/main/java/pl/edu/wszib/springhelloworld/MainApp.java
@@ -0,0 +1,11 @@
+package pl.edu.wszib.springhelloworld;
+
+import org.springframework.boot.SpringApplication;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelloWorldApplication.class, args);
+    }
+
+}
